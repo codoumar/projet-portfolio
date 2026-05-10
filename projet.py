88118e63537dbe0930daed76d2a2b64355c39896
codoumar <codoumarfaye3105@gmail.com>
2026-05-10 20:18:43 +0000

    Update projet.py

--- projet.py
+++ projet.py
@@ -39,16 +39,16 @@ col5,col6=st.columns(2)
 with col5:
     st.markdown(" J’ai déjà pratiqué la culture de tomates, en assurant leur plantation, leur suivi et leur entretien. Cette expérience m’a permis de développer des compétences pratiques en agriculture, notamment dans la gestion de la croissance, l’arrosage et la récolte, renforçant ainsi mon intérêt pour les productions végétales et les sciences agricoles")
 with col6:
-    st.image("culture de tomates.png", caption="Culture de tomates")
+    st.image("culture de tomates.png",width=150)
 col7,col8=st.columns(2)
 with col7:
     st.markdown("J’ai déjà cultivé de la menthe en prenant en charge sa plantation, son entretien et sa récolte. Cette pratique m’a permis d’acquérir un savoir-faire concret dans la gestion des plantes aromatiques, notamment en matière d’arrosage, de suivi de la croissance et de qualité de production.")
-with col8:
-    st.image("culture de menthe.jpeg",width=200,caption="culture de menthe")
+with col8:"
+    st.image("culture de menthe.jpeg",width=150)
         
 with st.sidebar:
     st.image("portrait.jpeg",width=200)
-    st.markdown("Mail:codoumarfaye3105@gmail.com")
+    st.markdown(" Mail:codoumarfaye3105@gmail.com")
     st.markdown("Dakar,sénégal")
     st.markdown("-------------------------------")
     st.markdown("Veuillez me joindre par mail pour toute information supplémentaire.")
