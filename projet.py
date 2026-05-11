6bd19381a127544555fe8180caff4b67c67a22ca
codoumar <codoumarfaye3105@gmail.com>
2026-05-11 15:21:22 +0000

    Update projet.py

--- projet.py
+++ projet.py
@@ -9,8 +9,8 @@ st.markdown("Ingénieur agronome passionnée par la production animale, avec des
 st.markdown("--------------------------------------------------------------------------")
 st.subheader("Parcours académique")
 st.markdown("""
--* 2024-2027:Licence en Sciences et Techniques Agricoles Alimentaires et Nutritionnelles,université Amadou Mahtar Mbow,Diamniadio.")
--* 2023-2024:Baccalauréat en Sciences expérimentales,Institution Saint Louis Marie Grignion,Malika.""")
+- **2024-2027**:Licence en Sciences et Techniques Agricoles Alimentaires et Nutritionnelles,université Amadou Mahtar Mbow,Diamniadio.")
+- **2023-2024**:Baccalauréat en Sciences expérimentales,Institution Saint Louis Marie Grignion,Malika.""")
 st.markdown("--------------------------------------------------------------------------")
 
 st.subheader("Compétences techniques")
